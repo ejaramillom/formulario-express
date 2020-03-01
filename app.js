5d3868c42880f670bc02c4722af725ca6430cd54
ejaramillom <ejaramilo@hotmail.com>
2020-03-01 17:30:44 -0500

    Added full views functions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/dbfive', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/userOne', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 // definimos el schema
 const userSchema = new mongoose.Schema({
@@ -15,19 +15,29 @@ const User = mongoose.model("User", userSchema);
 // definir las rutas app
 app.set('view engine', 'pug');
 app.set('views', 'views');
-app.set('view engine', 'pug');
-app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function(req, res){
-  res.render('index');
+  User.find(function(err, users){
+    let registeredUsers = [];
+    users.forEach(function(user){
+      registeredUsers.push(user);
+    });
+    res.render('index', {
+     registeredUsers: registeredUsers
+    });
+  });
 });
 app.get('/register', function(req, res){
-  res.render('index');
+  res.render('register');
 });
 app.post('/register', function(req, res){
   var nombre= req.body.nombre;
-  var nombre= req.body.email;
-  res.send('<h1> Hola ' + nombre + '!</h1>');
+  var email= req.body.email;
+  var password= req.body.password;
+  User.create({ name: name, email: count, password: password }, function( err ) {
+    if (err) return console.error( err );
+  });
+  res.redirect('/');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
