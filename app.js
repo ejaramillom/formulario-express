b8972b97c75f5fe4be3de8f44186c3347b95756e
ejaramillom <ejaramilo@hotmail.com>
2020-03-05 21:30:09 -0500

    fully working form

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
-const express = require('express');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require( "express" );
+const mongoose = require( "mongoose" );
+const bodyParser = require( "body-parser" );
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/userOne', { useNewUrlParser: true });
-mongoose.connection.on("error", function(e) { console.error(e); });
+mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost:27017/userOne", { useNewUrlParser: true });
+mongoose.connection.on( "error", function( e ) { console.error( e ); });
 // definimos el schema
 const userSchema = new mongoose.Schema({
   name: String,
   email: String
 });
 // definimos el modelo
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model( "User", userSchema );
 // definir las rutas app
-app.set('view engine', 'pug');
-app.set('views', 'views');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.get('/', function(req, res){
-  User.find(function(err, users){
-    let registeredUsers = [];
-    users.forEach(function(user){
-      registeredUsers.push(user);
-    });
-    res.render('index', {
+app.set( "view engine", "pug" );
+app.set( "views", "views" );
+app.use( bodyParser.urlencoded({ extended: false }));
+app.get( "/", async function( req, res ){
+  let registeredUsers = await User.find();
+    res.render( "index", {
      registeredUsers: registeredUsers
-    });
   });
 });
-app.get('/register', function(req, res){
-  res.render('register');
+
+app.get( "/register", async function(req, res){
+  res.render( "register" );
 });
-app.post('/register', function(req, res){
-  var nombre= req.body.nombre;
+app.post( "/register" ,async function(req, res){
+  var name= req.body.name;
   var email= req.body.email;
   var password= req.body.password;
-  User.create({ name: name, email: count, password: password }, function( err ) {
-    if (err) return console.error( err );
-  });
+  user = await User.create({ name: name, email: email, password: password });
+  user.save();
   res.redirect('/');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log( "Listening on port 3000!" ));
